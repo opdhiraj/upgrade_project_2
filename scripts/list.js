3321f9229b1c9113830fef916572ca9667e4089f
Om Prakash Dhiraj <82339776+opdhiraj@users.noreply.github.com>
2021-07-16 01:24:37 +0530

    Update list.js

--- scripts/list.js
+++ scripts/list.js
@@ -19,7 +19,7 @@
 fetch("https://travel-advisor.p.rapidapi.com/hotels/list-by-latlng?latitude=12.91285&longitude=100.87808&lang=en_US&hotel_class=1%2C2%2C3&limit=30&adults=1&amenities=beach%2Cbar_lounge%2Cairport_transportation&rooms=1&child_rm_ages=7%2C10&currency=USD&checkin=2020-01-08&zff=4%2C6&subcategory=hotel%2Cbb%2Cspecialty&nights=2", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-key": "16bb4a759cmsh97369a373ca8763p194038jsnaa21aab49247",
+		"x-rapidapi-key": "your api",
 		"x-rapidapi-host": "travel-advisor.p.rapidapi.com"
 	}
 })
